d8d57bbcb412d45d285eb11c1ce871b05387b973
Vlad Rimsha <vlad@jelly.ninja>
2025-08-11 12:50:52 +0700

    Update for remote mcp

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -12,17 +12,17 @@ export async function POST(req: Request) {
   }: { messages: UIMessage[]; model: string; webSearch: boolean } =
     await req.json();
 
-  // const transport = new StreamableHTTPClientTransport(new URL('http://localhost:3000/mcp'))
-  // const notion = await experimental_createMCPClient({
-  //   transport
-  // })
+  const transport = new StreamableHTTPClientTransport(new URL('https://notion-mcp-server-i5w6.onrender.com/mcp'))
+  const notion = await experimental_createMCPClient({
+    transport
+  })
 
-  // const tools = await notion.tools()
+  const tools = await notion.tools()
 
   const result = streamText({
     model: webSearch ? 'perplexity/sonar' : model,
     messages: convertToModelMessages(messages),
-    // tools,
+    tools,
     stopWhen: stepCountIs(5),
     system:
       'You are a helpful assistant that can answer questions and help with tasks',
