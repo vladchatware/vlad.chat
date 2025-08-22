2249a6483944490bbd9c31c9f1fc8414855767a0
Vlad Rimsha <vlad@jelly.ninja>
2025-08-22 21:53:20 +0700

    Fix padded overflow

--- app/page.tsx
+++ app/page.tsx
@@ -81,7 +81,7 @@ const ChatBotDemo = () => {
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 relative size-full h-screen">
+    <div className="max-w-4xl mx-auto m-6 relative size-full h-screen">
       <div className="flex flex-col h-full">
         <Conversation className="h-full">
           <ConversationContent>
