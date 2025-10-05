71861b271422d76df91dbe81551b629eaef491ef
Vlad Rimsha <vlad@jelly.ninja>
2025-10-05 15:09:01 +0700

    Integrate Posthog

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ['8f63fa0efd79.ngrok-free.app', 'localhost', 'vlad.chat'],
+  async rewrites() {
+    return [
+      {
+        source: '/ingest/static/:path*',
+        destination: 'https://us-assets.i.posthog.com/static/:path*',
+      },
+      {
+        source: '/ingest/:path*',
+        destination: 'https://us.i.posthog.com/:path*',
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true,
   /* config options here */
 };
 
