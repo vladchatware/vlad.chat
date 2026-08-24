9ccb5e026b9489e5dc962fefb37aaca8257c0310
vladchatware <703823+vladchatware@users.noreply.github.com>
2026-08-24 22:01:54 +0700

    fix: avoid PostHog flag timeout in development

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,7 +1,11 @@
 import posthog from "posthog-js"
 
+const apiHost = process.env.NODE_ENV === "development"
+  ? process.env.NEXT_PUBLIC_POSTHOG_HOST ?? "https://us.i.posthog.com"
+  : "/ingest"
+
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
+  api_host: apiHost,
   ui_host: "https://us.posthog.com",
   defaults: '2025-05-24',
   capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
